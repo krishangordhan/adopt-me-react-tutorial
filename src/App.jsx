1c06fd80ee0952caad4ecc9a1272a0b6ecc757f9
Krishan Gordhan <krishan-gordhan@idexx.com>
2024-03-31 09:17:32 +1300

    Adding in hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="Dog" breed="Havanese" />
-    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-    <Pet name="Doink" animal="Cat" breed="Mixed" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
